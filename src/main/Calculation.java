35458c72b4afd1d5f8bdc604ddf59e99086d0dc0
klafarul <41956123+klafarul@users.noreply.github.com>
2019-12-04 11:23:27 +0300

    corrections. LastResult - Result

--- src/main/Calculation.java
+++ src/main/Calculation.java
@@ -2,7 +2,10 @@ package main;
 
 import java.util.ArrayList;
 
-public class Results {
+public class Calculation {
+	/**
+	* этот массив хранит результат вычисления каждого потока, номер потока соответствует номеру элемента в массиве
+	**/
 	private ArrayList<Double> results;
 	
 	double begin, end;
@@ -11,15 +14,17 @@ public class Results {
 	double step;
 	
 
-	
-	public Results(int numberOfThreads){
+	/**
+	*конструктор
+	**/
+	public Calculation(double begin, double end, double alfa, int numberOfThreads){
 		results = new ArrayList<Double>();
 		for (int i = 0; i < numberOfThreads; i++){
 			results.add(0.0);
 		}
-		this.begin = 1;
-		this.end = 10;
-		this.alfa = 0.5;
+		this.begin = begin;
+		this.end = end;
+		this.alfa = alfa;
 		this.numberOfThreads = numberOfThreads;
 		this.step = (double)(this.end - this.begin) / numberOfThreads;
 		
@@ -28,41 +33,30 @@ public class Results {
 	*подинтегральная функция
 	**/
 	public double function(double x){
-		return Math.sqrt(2 + x*x);		
+		return x*x;		
 	}
 	
-	synchronized double getDelta(int threadNumber){
-		double result;
-		if (threadNumber != results.size() - 1){
-			result = Math.abs(results.get(threadNumber) - results.get(threadNumber + 1));
-		}
-		else{
-			result = Math.abs(results.get(threadNumber) - results.get(threadNumber - 1));
-		}
-		return result;
-		
-	}
+	
 	
 	synchronized void setResult(int threadNumber, double result){
-		results.set(threadNumber, result);				
-	}
-	
-
-
-	
-	
+		results.set(threadNumber, result);	
+	}	
+	/**
+	*Вычисляет интеграл на отрезке
+	**/
 	public double calcInt(double a, double b, int segments){
 
 		double result = 0.0;
 		double step = (double)(b - a) /(double) (segments);
 		
-		for (int i = 1; i <= segments; i++){
+		for (int i = 0; i < segments; i++){
 			result += function(a + step * (i + 0.5));			
 		}
 		
 			
 		return result * step;
 	}
+	
 	
 	public double getIntegral(){
 		double result = 0.0;
--- src/main/MyThread.java
+++ src/main/MyThread.java
@@ -7,25 +7,36 @@ public class MyThread implements Runnable{
 	
 	Thread thrd;
 	int number;
-	Results res;
+	Calculation res;
 	
-	public MyThread(int i, Results res){
+	
+	public MyThread(int number, Calculation res){
 		thrd = new Thread(this);
-		this.number = i;
+		this.number = number;
 		this.res = res;
 		thrd.start();
 		
 	}
-	
-	
+	/**
+	* Вычисляем интеграл подотрезка
+	**/
 	@Override
 	public void run(){
-		double result;
+		double currentResult;
+		double lastResult = 0;
 		int n = 10;
+		boolean end = false;
 		do{
-			result = res.calcInt(res.begin + number * res.step, res.begin + number * res.step + res.step, n);
-			res.setResult(number, result);
-			n+=100;			
-		} while (res.getDelta(number) >= res.alfa);
+			currentResult = res.calcInt(res.begin + number * res.step, res.begin + number * res.step + res.step, n);
+			if (Math.abs(currentResult - lastResult) >= res.alfa){
+				lastResult = currentResult;
+				n +=10;
+			}
+			else{
+				lastResult = currentResult;
+				end = true;
+			}
+			res.setResult(number, lastResult);						
+		} while (!end);
 	}
 } 
